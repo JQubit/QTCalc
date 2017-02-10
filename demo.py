fdfcdcbea006a7c2a30c7e65c46bb85e2247547c
SD <sddeath@gmail.com>
2017-02-10 20:11:19 +0300

    initial commit

---
+++ demo.py
@@ -0,0 +1,71 @@
+import progressbar
+
+from structure import Region, BandBended, Polarized
+from structure import Structure
+import numpy as np
+from scipy import constants as sc
+from typing import List
+from matplotlib import pyplot as plt
+import nu
+
+m = {'GaAs': 0.063,
+     'AlxGa1-xAs': lambda x: 0.063 + 0.083 * x,
+     'AlAs': 0.146,
+     'InN': 0.11,
+     'GaN': 0.20,
+     'InxGa1-xN': lambda x: 0.2 + x * (0.11 - 0.2),
+     'InAs': 0.023}
+
+if __name__ == '__main__':
+    plt.style.use('ggplot')
+
+    THICKNESS = 35
+    P = 1e-22
+    screening_length = 1e-9
+    EPS = 30
+    space_charge = THICKNESS * nu.nm * P / (EPS * 2 * screening_length + THICKNESS * nu.nm)
+    print(space_charge /P)
+    a = [
+        BandBended(
+            side=1,
+            screening_length=screening_length,
+            space_charge=space_charge,
+            potential=0.0,
+            effective_mass=0.063,
+            thickness=10
+        ),
+        Polarized(
+            potential=0.375,
+            effective_mass=m['AlxGa1-xAs'](.5),
+            thickness=THICKNESS),
+        BandBended(
+            side=0,
+            screening_length=screening_length,
+            space_charge=-space_charge,
+            potential=0.0,
+            effective_mass=0.063,
+            thickness=10
+        ),
+    ]
+    s = Structure(a, 200)
+    s.plot_structure()
+    E = np.linspace(-1.0, 1.0, 200) * nu.eV
+    STEPS = 30
+    V = np.linspace(-10, 10, STEPS)
+
+    a[0].set_space_charge(-space_charge)
+    a[2].set_space_charge(space_charge)
+    s.update_potentials()
+    I_minus = s.compute_IV(V, E, a[1])
+
+    a[0].set_space_charge(space_charge)
+    a[2].set_space_charge(-space_charge)
+    s.update_potentials()
+    I_plus = s.compute_IV(V, E, a[1])
+
+    plt.plot(V, I_plus, label="P+")
+    plt.plot(V, I_minus, label="P-")
+    plt.xlabel('V')
+    plt.ylabel('I')
+    plt.legend()
+    plt.show()
---
+++ structure.py
@@ -0,0 +1,250 @@
+import numpy as np
+import progressbar
+from scipy import constants as sc
+from typing import List
+from matplotlib import pyplot as plt
+
+import nu
+
+
+class Region:
+    NAME = 'N/A'
+    POTENTIAL = 0.0  # eV
+    EFFECTIVE_MASS = 1  # in e
+    THICKNESS = 10  # nm
+
+    def __init__(self, potential=None, effective_mass=None, thickness=None, dv=None):
+        self.dv = dv or 0
+        self.THICKNESS = thickness or self.THICKNESS
+        self.EFFECTIVE_MASS = effective_mass or self.EFFECTIVE_MASS
+        self.POTENTIAL = potential or self.POTENTIAL
+        self.next_region = None
+        self.prev_region = None
+
+    def get_applied_voltage(self):
+        return self.dv
+
+    def set_applied_voltage(self, value):
+        self.dv = value
+
+    def get_name(self):
+        return self.NAME
+
+    def get_potential(self, x):
+        # x in [0, 1]
+        return self.POTENTIAL * np.ones_like(x) * nu.eV
+
+    def get_thickness(self):
+        return self.THICKNESS * nu.nm
+
+    def get_effective_mass(self):
+        return self.EFFECTIVE_MASS * sc.m_e
+
+    def set_prev_region(self, v):
+        self.prev_region = v
+
+    def set_next_region(self, v):
+        self.next_region = v
+
+
+class Polarized(Region):
+    def __init__(self, polarization=1, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_potential(self, x):
+        # x in [0, 1]
+        phi_a = self.prev_region.get_potential(1)
+        phi_b = self.prev_region.get_potential(0)
+
+        return self.POTENTIAL * np.ones_like(x) * nu.eV + \
+               phi_a + (phi_b - phi_a) * x
+
+
+class BandBended(Region):
+    def __init__(self, space_charge=1, screening_length=1, side=1, sign=1, *args, **kwargs):
+        self.space_charge = space_charge
+        self.screening_length = screening_length
+        self.side = side
+        self.sign = sign
+        super().__init__(*args, **kwargs)
+
+    def set_space_charge(self, v):
+        self.space_charge = v
+
+    def get_potential(self, x):
+        # x in [0, 1]
+
+        return self.POTENTIAL * np.ones_like(x) * nu.eV + \
+               self.sign * self.space_charge * self.screening_length * np.exp(
+                   -np.abs(self.side - x) * self.get_thickness() / self.screening_length) / sc.epsilon_0
+
+
+class Structure:
+    def __init__(self, structure: List[Region], granularity: int):
+        self.structure = structure
+        for idx, s in enumerate(self.structure):
+            if idx != 0:
+                s.set_prev_region(self.structure[idx - 1])
+            if idx != len(self.structure) - 1:
+                print(idx)
+                s.set_next_region(self.structure[idx + 1])
+
+        self.granularity = granularity
+        self.x_grid = None
+        self.m_e = None
+        self.u = None
+        self.build_grid()
+
+    def compute_TC(self, E):
+        """Compute transmission coefficient in the energy range E
+
+        To compute transmission coefficient, piece wise constant method is
+        performed.
+
+        Args:
+            E: specify desired energy range in (J)
+            e.g. E = np.linspace(0, 1.0)*1.60e-19
+
+        Return:
+            TC: transmission coefficient (no unit)
+        """
+
+        U = self.u
+
+        x = self.x_grid
+        x = np.insert(x, 0, 0)
+        m_e = self.m_e
+        k = np.array([np.sqrt(2 * m_e * (e - U + 0j)) / sc.hbar
+                      for e in E])
+        cns_m_e = (m_e[1:] / m_e[:-1])
+        cns_k = (k[:, :-1] / k[:, 1:])
+        cns_m_e_k = cns_m_e * cns_k
+        M11 = (0.5 * (1 + cns_m_e_k) * np.exp(-1j * (k[:, 1:] - k[:, :-1]) * x[1:-1]))
+        M12 = (0.5 * (1 - cns_m_e_k) * np.exp(-1j * (k[:, 1:] + k[:, :-1]) * x[1:-1]))
+        M21 = (0.5 * (1 - cns_m_e_k) * np.exp(1j * (k[:, 1:] + k[:, :-1]) * x[1:-1]))
+        M22 = (0.5 * (1 + cns_m_e_k) * np.exp(1j * (k[:, 1:] - k[:, :-1]) * x[1:-1]))
+
+        m11, m12, m21, m22 = M11[:, -1], M12[:, -1], M21[:, -1], M22[:, -1]
+        for __ in range(len(U) - 2):
+            func = lambda m1, m2, m3, m4: m1 * m2 + m3 * m4
+            a = func(m11, M11[:, -__ - 2], m12, M21[:, -__ - 2])
+            b = func(m11, M12[:, -__ - 2], m12, M22[:, -__ - 2])
+            c = func(m21, M11[:, -__ - 2], m22, M21[:, -__ - 2])
+            d = func(m21, M12[:, -__ - 2], m22, M22[:, -__ - 2])
+            m11, m12, m21, m22 = a, b, c, d
+
+        MT22 = m22
+        ret = ((m_e[-1] / m_e[0]) * (k[:, 0] / k[:, -1]) *
+               (MT22 * np.conjugate(MT22)) ** -1)
+        TC = np.where(np.isnan(ret), 0, ret.real)
+        return TC
+
+    def compute_J(self, TC, E, T=273, Ef_left_shift=0, Ef_right_shift=0):
+        U = self.u
+        Ef_left = Ef_left_shift + U[0]
+        Ef_right = Ef_right_shift + U[-1]
+        m_e = self.m_e
+        m_left = m_e[0] * sc.m_e
+
+        def fermi_distribution(Ef, U):
+            return 1 / (np.exp((U - Ef) / (sc.k * T)) + 1)
+
+        coeff = sc.e * m_left / (2 * np.pi ** 2 * sc.hbar ** 3)
+        delta_fermi = lambda E: fermi_distribution(Ef_left, E) - fermi_distribution(Ef_right, E)
+        delta_fermi_net = fermi_distribution(Ef_left, E) * (1 - fermi_distribution(Ef_right, E))
+        dE = E[1] - E[0]
+
+        # plt.plot(E, fermi_distribution(Ef_left, E) - fermi_distribution(Ef_right, E))
+        # plt.plot(E, delta_fermi_net)
+        # plt.show()
+        # exit()
+        def outer_integration(idx):
+            # inner_integral = quad(delta_fermi, Ex, np.inf)
+            inner_integral_net = np.trapz(delta_fermi_net[idx:], dx=dE)
+
+            return inner_integral_net
+
+        outer_integration_y = np.array([outer_integration(idx) for idx, D in enumerate(TC)])
+
+        # plt.plot(outer_integration_y)
+        # plt.plot(TC)
+        # plt.show()
+        # plt.plot(outer_integration_y*TC)
+        # plt.show()
+        J = np.trapz(outer_integration_y * TC, dx=dE) * coeff
+
+        return J
+
+    def build_grid(self):
+        N = self.granularity
+        x = np.array([])
+        m_e = np.array([])
+        U = []
+        x_grid = []
+        x_depth = 0
+        u_depth = 0
+
+        rel_x = np.linspace(0, 1, num=N)
+        for region in self.structure:
+            assert isinstance(region, Region)
+            x_grid.append(np.linspace(x_depth, x_depth + region.get_thickness(), num=N,
+                                      endpoint=False))
+            x_depth += region.get_thickness()
+            U.append(region.get_potential(rel_x) + u_depth + (rel_x * region.get_applied_voltage()) * nu.eV)
+            u_depth += region.get_applied_voltage() * nu.eV
+
+        x_grid = np.array(x_grid).flatten()
+        x_grid = np.append(x_grid, x_depth)
+        x_grid = np.delete(x_grid, 0)
+        self.x_grid = x_grid
+        self.m_e = np.append(m_e, [np.ones(N) * region.get_effective_mass() for region in self.structure])
+        self.u = np.array(U).flatten()
+
+    def update_potentials(self):
+        u_depth = 0
+        N = self.granularity
+        rel_x = np.linspace(0, 1, num=N)
+        d = 0
+        for region in self.structure:
+            self.u[d: d + N] = region.get_potential(rel_x) + u_depth + (rel_x * region.get_applied_voltage()) * nu.eV
+            u_depth += region.get_applied_voltage() * nu.eV
+            d += N
+
+    def plot_structure(self):
+        """Plot potential structure
+        """
+        U = self.u
+        x = self.x_grid
+        # double up for plotting purpose
+        x = np.ravel(np.dstack((x, x)))
+        x = np.insert(x, 0, 0)
+        x = np.delete(x, -1)
+        U = np.ravel(np.dstack((U, U)))
+        # set up max and min value for plotting purpose
+        Vmax = np.max(U)
+        Vmax = 1.05 * Vmax / sc.e
+        Vmin = np.min(U)
+        Vmin = 1.05 * Vmin / sc.e
+        xmin = x[0] / nu.nm
+        xmax = x[-1] / nu.nm
+        # plot
+        plt.plot(x / nu.nm, U / nu.eV)
+        plt.grid(True)
+        plt.xlabel('position (nm)')
+        plt.ylabel('potential (eV)')
+        plt.xlim(xmin, xmax)
+        margin = 0.1 * np.abs(Vmax)
+        plt.ylim(Vmin - margin, Vmax + margin)
+        plt.show()
+
+    def compute_IV(self, V, E, a):
+        I_minus = np.zeros_like(V)
+        bar = progressbar.ProgressBar(max_value=len(V))
+        for idx, v in enumerate(V):
+            a.set_applied_voltage(v)
+            self.update_potentials()
+            TC = self.compute_TC(E)
+            J = self.compute_J(TC, E)
+            I_minus[idx] = J
+            bar.update(idx)
+        return I_minus
